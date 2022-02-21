9676f23ec4a5eee47803fe0e5c33d83391021f0d
ErikaSarleti <erikasarleti00@gmail.com>
2022-02-21 16:07:56 +0100

    Esercizio finito (ho scritto male la vecchia repo)

---
+++ myscript.js
@@ -0,0 +1,12 @@
+let userAge= prompt("Quanti anni hai?");
+let travel= prompt("Quanti Km devi percorrere?");
+let kmPrice= 0.21;
+let priceRes =document.getElementById("price-result");
+
+if (userAge < 18){
+    priceRes.innerHTML = `${((travel*kmPrice) - (travel*kmPrice * 20 / 100)).toFixed(2)}€`;
+}else if (userAge > 65){
+        priceRes.innerHTML = `${((travel*kmPrice) - (travel*kmPrice * 40 / 100)).toFixed(2)}€`;
+}else{
+    priceRes.innerHTML = `${(travel*kmPrice).toFixed(2)}€`;
+}
