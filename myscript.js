a22706838c96e80835e90f94cdc77e3e0e39fba6
ErikaSarleti <erikasarleti00@gmail.com>
2022-02-21 17:29:32 +0100

    fixato over 65

--- myscript.js
+++ myscript.js
@@ -5,7 +5,7 @@ let priceRes =document.getElementById("price-result");
 
 if (userAge < 18){
     priceRes.innerHTML = `${((travel*kmPrice) - (travel*kmPrice * 20 / 100)).toFixed(2)}€`;
-}else if (userAge > 65){
+}else if (userAge >= 65){
         priceRes.innerHTML = `${((travel*kmPrice) - (travel*kmPrice * 40 / 100)).toFixed(2)}€`;
 }else{
     priceRes.innerHTML = `${(travel*kmPrice).toFixed(2)}€`;
