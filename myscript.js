add16488163088f9e376a7899df3629c536ea7b5
ErikaSarleti <erikasarleti00@gmail.com>
2022-02-22 10:36:27 +0100

    miglioramenti

--- myscript.js
+++ myscript.js
@@ -1,12 +1,14 @@
 let userAge= prompt("Quanti anni hai?");
 let travel= prompt("Quanti Km devi percorrere?");
-let kmPrice= 0.21;
+const kmPrice= 0.21;
+const discountMinors= 20;
+const discountElders= 40;
 let priceRes =document.getElementById("price-result");
 
 if (userAge < 18){
-    priceRes.innerHTML = `${((travel*kmPrice) - (travel*kmPrice * 20 / 100)).toFixed(2)}€`;
+    priceRes.innerHTML = `${((travel*kmPrice) - (travel*kmPrice * discountMinors / 100)).toFixed(2)}€`;
 }else if (userAge >= 65){
-        priceRes.innerHTML = `${((travel*kmPrice) - (travel*kmPrice * 40 / 100)).toFixed(2)}€`;
+        priceRes.innerHTML = `${((travel*kmPrice) - (travel*kmPrice * discountElders / 100)).toFixed(2)}€`;
 }else{
     priceRes.innerHTML = `${(travel*kmPrice).toFixed(2)}€`;
 }
